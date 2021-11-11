5a6823b63f98666f3a69dbc3ffb93e2e5d18b66c
EC2 Default User <ec2-user@ip-172-31-24-183.eu-west-2.compute.internal>
2021-11-11 23:41:01 +0000

    updated home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <Tutorial/>
+  <div>
+<h1 class="text-4xl text-center my-8 uppercase">Welcome to SvelteKit Pokedex</h1>
+<input class="rounded-md w-full text-lg p-4 border-2
+    border-gray-200" type="text" placeholder="Search for Pokemon" bind:value={searchTerm}>
+<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+// put pokemon here
+</div>
+</div>
 </template>
 
 <script>
