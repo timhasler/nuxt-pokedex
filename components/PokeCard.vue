2f29eb6dd6361a393e6c3ef02556bdc37e9d1778
EC2 Default User <ec2-user@ip-172-31-24-183.eu-west-2.compute.internal>
2021-11-13 00:08:07 +0000

    Initial Pokemon Load

---
+++ components/PokeCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="list-none p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center">
+        <img class="h-40 w-40 " :src="pokemon.image" :alt="pokemon.name" />
+        <h2 class="uppercase text-2xl">{{ pokemon.id }}:  {{ pokemon.name }}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            pokemon: {
+                default: () => {},
+                type: Object
+            }
+        }
+    }
+</script>§
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,45 @@
 <template>
   <div>
-<h1 class="text-4xl text-center my-8 uppercase">Welcome to SvelteKit Pokedex</h1>
-<input class="rounded-md w-full text-lg p-4 border-2
-    border-gray-200" type="text" placeholder="Search for Pokemon" bind:value={searchTerm}>
-<div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
-// put pokemon here
-</div>
-</div>
+    <h1 class="text-4xl text-center my-8 uppercase">Welcome to SvelteKit Pokedex</h1>
+    <input class="rounded-md w-full text-lg p-4 border-2 border-gray-200"
+      type="text" placeholder="Search for Pokemon" bind:value={searchTerm}>
+    <div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
+        <PokeCard 
+          v-for="pokemon in allPokemon" 
+          :key="pokemon.id"
+          :pokemon="pokemon"
+        />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import PokeCard from '~/components/PokeCard'
+
+export default {
+  components: {
+    PokeCard
+  },
+  
+  data() {
+    return {
+      allPokemon: []
+    }
+  },
+  
+  async fetch () {
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=151`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const pokemon = await data.results.map((data, index) => ({
+      name: data.name,
+      id: index + 1,
+      image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+        index + 1
+      }.png`
+    }));
+    
+    this.allPokemon = pokemon;
+  }
+}
 </script>
